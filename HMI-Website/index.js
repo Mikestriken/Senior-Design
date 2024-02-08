c0984f0d2a9a55d4e12c3d7cb6ed80451861e488
Michael <michael.john.marais379@gmail.com>
2024-02-08 14:38:51 +0000

    added preliminary JS

---
+++ HMI-Website/index.js
@@ -0,0 +1,24 @@
+const batteryPercentage = document.getElementById("batteryPercentageOutput");
+const battery = document.getElementById("batteryGraphicOutput");
+
+let i = 0;
+
+function updateBattery() {
+    if (i <= 100) {
+        battery.style.width = i + '%';
+        batteryPercentage.innerHTML = i + '%';
+        i++;
+    } else {
+        clearInterval(intervalId); // Stop the interval when battery reaches 100%
+    }
+}
+
+const intervalId = setInterval(updateBattery, 100); // Run updateBattery every 1000 milliseconds
+
+/* for (let i=0; i<=100; ++i)
+{
+    setInterval(function(){
+        battery.style.width = i + '%';
+        batteryPercentage.innerHTML = i + '%';
+    },1000);
+} */
