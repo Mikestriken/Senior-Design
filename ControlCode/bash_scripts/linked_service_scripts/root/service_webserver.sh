674453db1d893bbbf59abbf7e46e5a3901afd7d1
Michael <michael.john.marais379@gmail.com>
2024-03-08 04:39:15 +0000

    updated script pathing to use python modules

--- ControlCode/bash_scripts/linked_service_scripts/root/service_webserver.sh
+++ ControlCode/bash_scripts/linked_service_scripts/root/service_webserver.sh
@@ -6,5 +6,8 @@ script_dir=$(dirname "$(readlink -f "$0")")
 # Change the current directory to the script's directory
 cd "$script_dir"
 
+# Change the directory to ControlCode
+cd "../../../"
+
 # Execute the flask webserver
-../../../../.venv/bin/py ../../../flask_webserver/flask_webserver.py --no-camera
+../venv/bin/python -m flask_webserver.flask_webserver --no-camera
