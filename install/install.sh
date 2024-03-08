e2715f328c9142eb365c976c9ed840751d684d64
Michael <michael.john.marais379@gmail.com>
2024-03-08 20:47:10 +0000

    added OS detection to avoid installing services on a windows OS to install.sh

--- install/install.sh
+++ install/install.sh
@@ -7,10 +7,28 @@ script_dir=$(dirname "$(readlink -f "$0")")
 # Change the current directory to the script's directory
 cd "$script_dir"
 
+# * Check current OS
+windows=false
+OS=$(uname)
+# Check if the output of uname is not "Linux"
+if [ "$OS" != "Linux" ]; then
+    # If it's not Linux, set windows=true
+    windows=true
+else
+    # Otherwise, set windows=false or leave it unset (your choice)
+    windows=false
+fi
+
+# * Run all the installation scripts
+
 repairScripts.sh
 
 installAndRepairAPTInstalls.sh
 
 installAndRepairVEnv.sh
 
-installAndRepairServices.sh
+if [ windows != true ]; then # Only install services on linux environment
+
+    installAndRepairServices.sh
+
+fi
