3055d5237a90b40ff6bd8a725872d83285d823bf
Michael <michael.john.marais379@gmail.com>
2024-03-08 04:24:31 +0000

    added restartServices.sh

---
+++ ControlCode/bash_scripts/developer_service_scripts/restartServices.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# * CD to the script file location
+# Get the directory where the script is located
+script_dir=$(dirname "$(readlink -f "$0")")
+
+# Change the current directory to the script's directory
+cd "$script_dir"
+
+# * Define variables that will be used
+# Source directories
+user_dir="../../service_files/user"
+root_dir="../../service_files/root"
+
+# Destination directories
+user_destination_dir="$HOME/.config/systemd/user/"
+root_destination_dir="/etc/systemd/system/"
+
+# Arrays to store file names
+user_services=()
+user_services+=("$user_dir"/*) # add the names of all the files in $user_dir/ to the user_services array
+
+root_services=()
+root_services+=("$root_dir"/*) # add the names of all the files in $root_dir/ to the user_services array
+
+# * Restart the services
+# Restart the user services
+echo -e "\nRestarting User Services..."
+for service_file in "${user_services[@]}"; do
+    if ! systemctl --user restart "$(basename "$service_file" .service)"; then
+        echo "Error: Restarting $(basename "$service_file" .service) service failed."
+        exit 1
+    fi
+done
+echo "done!"
+
+# Restart the root services
+echo -e "\nRestarting Root Services..."
+for service_file in "${root_services[@]}"; do
+    if ! sudo systemctl restart "$(basename "$service_file" .service)"; then
+        echo "Error: Restarting $(basename "$service_file" .service) service failed."
+        exit 1
+    fi
+done
+echo -e "done!\n"
